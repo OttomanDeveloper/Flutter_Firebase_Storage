a883a7c710d22997092b74fdecd37d06130d1252
Muhammad Usman <71749423+OttomanDeveloper@users.noreply.github.com>
2021-04-21 22:11:18 +0500

    OttomanCoder Tutorial

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String fileUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              children: [
+                MaterialButton(
+                  color: Colors.red,
+                  onPressed: () => uploadImage(),
+                  child: Text(
+                    'Upload Image',
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                fileUrl == null
+                    ? SizedBox()
+                    : Image.network(
+                        fileUrl,
+                        height: 200,
+                        width: double.infinity,
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> uploadImage() async {
+    final _fireStorage = FirebaseStorage.instance;
+    final image = ImagePicker();
+    PickedFile pickedFile;
+
+    // Request Photos Permission
+    await Permission.photos.request();
+    var permissionStatus = await Permission.photos.status;
+
+    // Checking Permission
+    if (permissionStatus.isGranted) {
+      pickedFile = await image.getImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        var file = File(pickedFile.path);
+        // Getting File Path
+        String fileName = file.uri.path.split('/').last;
+
+        // Uploading Image to FirebaseStorage
+        var filePath = await _fireStorage
+            .ref()
+            .child('demo/$fileName')
+            .putFile(file)
+            .then((value) {
+          return value;
+        });
+        // Getting Uploaded Image Url
+        String downloadUrl = await filePath.ref.getDownloadURL();
+        fileUrl = downloadUrl;
+        setState(() {});
+      } else {
+        print('No Image Selected');
+      }
+    } else {
+      print('Provider Permission');
+    }
+  }
+}
